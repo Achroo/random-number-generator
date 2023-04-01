b7f29a749846b5aa0bf4730599b231b69faa6dc4
Akhro <49365088+Achroo@users.noreply.github.com>
2023-04-01 21:26:47 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,26 @@
         <h1 class="text-5xl font-sans pt-8 pb-8">Dice App</h1>
         <h2 class="text-2xl font-sans pb-6">Choose the type of dice</h2>
         <div class="flex justify-around buttons-container">
-            <DiceButton text="D3"></DiceButton>
-            <DiceButton text="D4"></DiceButton>
-            <DiceButton text="D6"></DiceButton>
-            <DiceButton text="D10"></DiceButton>
-            <DiceButton text="D12"></DiceButton>
-            <DiceButton text="D20"></DiceButton>
+            <DiceButton v-on:click="setDiceType(3)" text="D3"></DiceButton>
+            <DiceButton v-on:click="setDiceType(4)" text="D4"></DiceButton>
+            <DiceButton v-on:click="setDiceType(6)" text="D6"></DiceButton>
+            <DiceButton v-on:click="setDiceType(10)" text="D10"></DiceButton>
+            <DiceButton v-on:click="setDiceType(12)" text="D12"></DiceButton>
+            <DiceButton v-on:click="setDiceType(20)" text="D20"></DiceButton>
         </div>
 
         <h2 class="text-2xl font-sans pt-8 pb-6">Choose the number of dices</h2>
         <div class="flex justify-around buttons-container pb-8">
-            <DiceButton text="1"></DiceButton>
-            <DiceButton text="2"></DiceButton>
-            <DiceButton text="3"></DiceButton>
-            <DiceButton text="4"></DiceButton>
-            <DiceButton text="5"></DiceButton>
-            <DiceButton text="6"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(1)" text="1"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(2)" text="2"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(3)" text="3"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(4)" text="4"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(5)" text="5"></DiceButton>
+            <DiceButton v-on:click="setDiceNumber(6)" text="6"></DiceButton>
         </div>
 
         <div class="buttons-container pb-8">
-          <button class="mx-auto w-28 h-16 rounded-lg bg-red-700 hover:bg-red-800">
+          <button v-on:click="rollDice()" class="mx-auto w-28 h-16 rounded-lg bg-red-700 hover:bg-red-800">
               <h1 class=" text-xl font-sans">Roll</h1>
           </button>
         </div>
@@ -37,6 +37,20 @@ export default {
   name: 'App',
   components: {
     DiceButton
+  },
+  methods : {
+    rollDice() {
+      console.log('Rolling dice')
+      console.log(this.diceType, this.diceNumber)
+    },
+    setDiceType(type) {
+      console.log('Setting dice type ' + type)
+      this.diceType = type
+    },
+    setDiceNumber(number) {
+      console.log('Setting dice number ' + number)
+      this.diceNumber = number
+    }
   }
 }
 </script>
